4eb1eae45a4dd11a8d8c74df19361088bbaec84f
mvu1996 <67136001+mvu1996@users.noreply.github.com>
2021-08-28 18:36:12 -0500

    cambios en el servidor
    errores y resoluciones

---
+++ angular-mysql-crud/server/build/keys.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    //encargado de configuracion
+    dataase: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'bd_FabricaTazas'
+    }
+};
