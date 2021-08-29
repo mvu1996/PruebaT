5184dfe94aaa3401202804e3c6bfb523b6d0f727
mvu1996 <67136001+mvu1996@users.noreply.github.com>
2021-08-29 12:32:14 -0500

    Angular
    actividad para angular.

--- angular-mysql-crud/client/src/app/app-routing.module.ts
+++ angular-mysql-crud/client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FormListComponent} from "./components/form-list/form-list.component"
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/ingresos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'games',
+    component: FormListComponent,
 
-const routes: Routes = [];
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
